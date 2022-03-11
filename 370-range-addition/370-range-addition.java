1a3b20c1663885c4c12b3d4c584a5e9b06b1d77a
ToAotao <33048685+ToAotao@users.noreply.github.com>
2022-03-11 13:57:28 -0800

    Time: 8 ms (36.62%), Space: 58.6 MB (5.46%) - LeetHub

--- 370-range-addition/370-range-addition.java
+++ 370-range-addition/370-range-addition.java
@@ -1,14 +1,39 @@
+class Difference {
+    private int[] diff;
+    public Difference(int[] arr) {
+        assert arr.length > 0;
+        diff = new int[arr.length];
+        diff[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            diff[i] = arr[i] - arr[i -1]; 
+        }
+    }
+    
+    public void increment(int i, int j, int val) {
+        diff[i] += val;
+        if (j + 1 < diff.length) {
+            diff[j + 1] -= val;
+        }
+    }
+    
+    public int[] result() {
+        int[] res = new int[diff.length];
+        res[0] = diff[0];
+        for (int i = 1; i < diff.length; i ++) {
+            res[i] = res[i - 1] + diff[i];
+            
+        }
+        return res;
+    }
+}
 class Solution {
     public int[] getModifiedArray(int length, int[][] updates) {
         int[] arr = new int[length];
+        Difference diff = new Difference(arr);
         for (int i = 0; i < updates.length; i++) {
-            int startIdx = updates[i][0];
-            int endIdx = updates[i][1];
-            int inc = updates[i][2];
-            for(int j = startIdx; j <= endIdx; j++) {
-                arr[j] += inc;
-            }
+            diff.increment(updates[i][0], updates[i][1], updates[i][2]);
         }
-        return arr;
+
+        return diff.result();
     }
 }
