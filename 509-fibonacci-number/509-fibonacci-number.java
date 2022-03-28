817fb541e835040696d7369a5516d640eec2ea39
ToAotao <33048685+ToAotao@users.noreply.github.com>
2022-03-27 17:47:07 -0700

    Time: 0 ms (100.00%), Space: 41.9 MB (5.36%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,7 +1,12 @@
 class Solution {
     public int fib(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        return fib(n - 1) + fib(n - 2);
+        int[] helper = new int[n + 1];
+        return job(helper, n);
+    }
+    public int job(int[] helper, int n) {
+        if (n == 0 || n == 1) return n;
+        if (helper[n] != 0) return helper[n];
+        helper[n] = job(helper, n - 1) + job(helper, n - 2);
+        return helper[n];
     }
 }
