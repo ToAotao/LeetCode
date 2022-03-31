b42492476eef07d0504fbcd825e74730c302e780
ToAotao <33048685+ToAotao@users.noreply.github.com>
2022-03-31 00:31:31 -0700

    Time: 1 ms (96.78%), Space: 44.9 MB (8.89%) - LeetHub

--- 174-dungeon-game/174-dungeon-game.java
+++ 174-dungeon-game/174-dungeon-game.java
@@ -1,28 +1,25 @@
 class Solution {
-    int[][] memo;
     public int calculateMinimumHP(int[][] dungeon) {
-        memo = new int[dungeon.length][dungeon[0].length];
+        int[][] memo = new int[dungeon.length][dungeon[0].length];
         for (int[] row : memo) {
             Arrays.fill(row, -1);
         }
-        return dp(0,0,dungeon);
+        return dp(0, 0, dungeon, memo);
         
     }
-    public int dp(int i , int j, int[][] dungeon ) {
-        int m  = dungeon.length;
+    public int dp(int i, int j, int[][] dungeon, int[][] memo){
+        int m = dungeon.length;
         int n = dungeon[0].length;
-        if (i == m - 1 && j == n - 1) {
+        if (i == m || j == n) return Integer.MAX_VALUE;
+        if(i == m - 1 && j == n - 1) {
             return dungeon[i][j] >= 0? 1 : -dungeon[i][j] + 1;
         }
-        if (i == m || j == n) {
-            return Integer.MAX_VALUE;
-        }
-        
-
         if (memo[i][j] != -1) return memo[i][j];
-        int res = Math.min(dp(i + 1, j, dungeon), dp(i, j+1, dungeon)) - dungeon[i][j];
-        memo[i][j] = res <= 0? 1 : res;
+        int res = Math.min(
+            dp(i + 1, j, dungeon, memo),
+            dp(i, j + 1, dungeon, memo)
+        ) - dungeon[i][j];
+        memo[i][j] = res <=0 ? 1 : res;
         return memo[i][j];
     }
-
 }
