3567e7971e4661520fd2178b00022f229f662576
ToAotao <33048685+ToAotao@users.noreply.github.com>
2022-04-15 14:16:57 -0700

    Time: 28 ms (87.36%), Space: 42.5 MB (86.11%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        String res = "";
+        for (int i = 0; i < s.length(); i++) {
+            String s1 = palindrome(s, i, i);
+            String s2 = palindrome(s, i, i + 1);
+            res = res.length() < s1.length()? s1 : res;
+            res = res.length() < s2.length()? s2 : res;
+        }
+        return res;
+    }
+    public String palindrome(String s, int l, int r) {
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)){
+            l--;
+            r++;
+        }
+        return s.substring(l + 1, r);
+    }
+}
