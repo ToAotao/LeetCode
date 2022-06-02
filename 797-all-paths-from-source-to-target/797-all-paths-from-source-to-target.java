45061c008044bedf9c81e46591eb2dcf0c653f84
ToAotao <33048685+ToAotao@users.noreply.github.com>
2022-06-01 23:37:06 -0700

    Time: 9 ms (22.01%), Space: 55.2 MB (35.90%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -1,21 +1,17 @@
 class Solution {
-    List<List<Integer>> res = new LinkedList<>();
+    List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        int n = graph.length - 1;
         LinkedList<Integer> path = new LinkedList<>();
-        traverse(graph, 0, path);
+        findPath(n, path, 0, graph);
         return res;
-    }
-    public void traverse(int[][] graph, int s, LinkedList<Integer> path) {
-         path.addLast(s);
-        int n = graph.length;
-        if (s == n - 1) {
-            res.add(new LinkedList<>(path));
-            path.removeLast();
-            return;
-        }
         
-        for (int v : graph[s]) {
-            traverse(graph, v, path);
+    }
+    public void findPath(int n, LinkedList<Integer> path, int j, int[][] graph) {
+        path.addLast(j);
+        if (n == j) res.add(new LinkedList<Integer>(path));
+        for (int i = 0; i < graph[j].length; i++) {
+            findPath(n, path, graph[j][i], graph);
         }
         path.removeLast();
     }
