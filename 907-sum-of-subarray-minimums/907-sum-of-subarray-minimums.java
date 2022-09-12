b8e6dffb49c5367211967e0e65ce85d26607e4f8
ToAotao <33048685+ToAotao@users.noreply.github.com>
2022-09-12 02:36:20 -0700

    Time: 19 ms (97.61%), Space: 49.8 MB (93.55%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.java
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.java
@@ -1,23 +1,21 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-          long res = 0;
+        long result = 0;
         int len = arr.length;
         int mod = (int)1e9 + 7;
-        //cc
-        if (arr == null || len == 0) return (int)res;
+        if(arr == null || arr.length == 0) return (int)result;
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i <= len; i++){
+            int cur = (i < len) == true? cur = arr[i] : -1;
         
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i <= len; i++) {
-            int curr = i < len ? arr[i] : -1;
-            
-            while (!stack.isEmpty() && arr[stack.peek()] > curr) {
+            while (!stack.isEmpty() && arr[stack.peek()] > cur){
                 int right = stack.pop();
-                int left = stack.isEmpty() == true ? -1 : stack.peek();
-                res += (long)(right - left) * (i - right) * arr[right];
+                int left = stack.isEmpty()? -1 : stack.peek();
+                result += arr[right] * (long)(right - left) * (i - right);
             }
             stack.push(i);
         }
-        res = res % mod;
-        return (int)res;
+        result = result % mod;
+        return (int)(result);
     }
 }
